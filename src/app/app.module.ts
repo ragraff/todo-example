47094f74846f1f1b81f3c184a42c7bc36a5f8aea
Richard Graff <ragraff@gmail.com>
2020-11-17 22:30:35 -0600

    Update app.module.ts
    using a const instead of a function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm/dist/interfaces/typeorm-options.interface';
 import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
 import { TodoModule } from '../resources/todo/todo.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-function getTypeOrmOptions(): Record<string, unknown> {
-  return {
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'changeme',
-    database: 'todo-example',
-    synchronize: true,
-    autoLoadEntities: true,
-    legacySpatialSupport: false,
-  };
-}
+const getTypeOrmOptions: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'changeme',
+  database: 'todo-example',
+  entities: ['../resources/**/entities/*.entity{.ts,.js}'],
+  synchronize: true,
+  autoLoadEntities: true,
+  logging: true,
+  legacySpatialSupport: false,
+  dropSchema: true,
+};
 
 @Module({
-  imports: [TodoModule, TypeOrmModule.forRoot(getTypeOrmOptions())],
+  imports: [TodoModule, TypeOrmModule.forRoot({ ...getTypeOrmOptions })],
   controllers: [AppController],
   providers: [AppService],
 })

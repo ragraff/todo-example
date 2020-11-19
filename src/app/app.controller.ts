352218e6962eeb776cda032ead86239f744c0b7d
Richard Graff <ragraff@gmail.com>
2020-11-18 21:41:57 -0600

    adding a simple auth endpoint

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../shared/auth/guards/jwt-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getRunning(): string {
+    return 'Api running.';
+  }
+
+  @Get('auth')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  getRunningAuth(): string {
+    return 'Api running with auth.';
   }
 }
